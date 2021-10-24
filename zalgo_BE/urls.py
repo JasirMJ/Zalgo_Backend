f6e9210fa5b2c27f9f88894b30259a0308332165
JASIR MOHAMMED <jasirmj@gmail.com>
2021-10-24 18:35:48 +0530

    media path added 1

--- zalgo_BE/urls.py
+++ zalgo_BE/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('UserApp.urls')),
