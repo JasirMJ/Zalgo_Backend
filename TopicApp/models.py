ee8af3ebe8de65c0f1d247510581954efb988f8d
JASIR MOHAMMED <jasirmj@gmail.com>
2021-11-03 00:04:08 +0530

    topic url modified

--- TopicApp/models.py
+++ TopicApp/models.py
@@ -7,6 +7,6 @@ class Topic(models.Model):
     duration = models.CharField(max_length=50,null=True)
     file = models.FileField(null=False,blank=False)
     description = models.TextField(null=True)
-    url = models.URLField(max_length=255,null=True)
+    url = models.TextField(max_length=255,null=True)
     priority = models.IntegerField(null=True)
     is_active = models.BooleanField(default=True)
