368b2082396428bb76ea3f53491aac4bd8cd7797
JASIR MOHAMMED <jasirmj@gmail.com>
2022-03-25 17:42:20 +0530

    Paytm checksum bug fixed

--- PaytmApp/views.py
+++ PaytmApp/views.py
@@ -39,7 +39,7 @@ class InitiateTransactionAPI(ListAPIView):
 
 
             MerchantID = self.request.POST.get("MerchantID","XEGbPk68721787257649")
-            MerchantKey = self.request.POST.get("","hFxAOaR_RPgkR7LZ")
+            MerchantKey = self.request.POST.get("MerchantKey","hFxAOaR_RPgkR7LZ")
             TXNAmount = self.request.POST['TXNAmount']
             Currency = self.request.POST.get("Currency","INR")
             websiteName = self.request.POST.get("websiteName","WEBSTAGING") # or DEFAULT
@@ -87,6 +87,85 @@ class InitiateTransactionAPI(ListAPIView):
             # for Production
             # url = "https://securegw.paytm.in/theia/api/v1/initiateTransaction?mid=YOUR_MID_HERE&orderId=ORDERID_98765"
             response = requests.post(url, data=post_data, headers={"Content-type": "application/json"}).json()
+            print("Paytm Initiate Response ",response)
+            return ResponseFunction(1, msg, {"request_data":self.request.data,"response":response})
+        except Exception as e:
+            msg = "Excepction @ InitiateTransactionAPI "+ printLineNo()+ " : "+ str(e)
+
+            return ResponseFunction(0,msg,{})
+
+
+class TransactionStatusAPI(ListAPIView):
+
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+    def post(self, request):
+        # required = ["name","country_code"]
+        # validation_errors = ValidateRequest(required, self.request.data)
+        #
+        # if len(validation_errors) > 0:
+        #     return ResponseFunction(0, validation_errors[0]['error'],{})
+        # else:
+        #     print("Receved required Fields")
+
+
+        try:
+
+            msg="initiated "
+            paytmParams = dict()
+
+            MerchantID = self.request.POST.get("MerchantID","XEGbPk68721787257649")
+            MerchantKey = self.request.POST.get("","hFxAOaR_RPgkR7LZ")
+            TXNAmount = self.request.POST['TXNAmount']
+            Currency = self.request.POST.get("Currency","INR")
+            websiteName = self.request.POST.get("websiteName","WEBSTAGING") # or DEFAULT
+            orderId = self.request.POST.get("orderId","1234")
+            custId = self.request.POST.get("custId","1234")
+
+            import requests
+            import json
+
+            # import checksum generation utility
+            # You can get this utility from https://developer.paytm.com/docs/checksum/
+            import PaytmChecksum
+
+            # initialize a dictionary
+            paytmParams = dict()
+
+            # body parameters
+            paytmParams["body"] = {
+
+                # Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+                "mid": MerchantID,
+
+                # Enter your order id which needs to be check status for
+                "orderId": orderId,
+            }
+
+            # Generate checksum by parameters we have in body
+            # Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+            checksum = PaytmChecksum.generateSignature(json.dumps(paytmParams["body"]), "YOUR_MERCHANT_KEY")
+
+            # head parameters
+            paytmParams["head"] = {
+
+                # put generated checksum value here
+                "signature": checksum
+            }
+
+            # prepare JSON string for request
+            post_data = json.dumps(paytmParams)
+
+            # for Staging
+            url = "https://securegw-stage.paytm.in/v3/order/status"
+
+            # for Production
+            # url = "https://securegw.paytm.in/v3/order/status"
+
+            response = requests.post(url, data=post_data, headers={"Content-type": "application/json"}).json()
+
             print("Paytm Initiate Response ",response)
             return ResponseFunction(1, msg, {"request_data":self.request.data,"response":response})
         except Exception as e:
