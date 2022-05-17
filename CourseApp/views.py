096b0e14dae1bb3cc523432da961dd02ee4aee62
JASIR MOHAMMED <jasirmj@gmail.com>
2022-05-18 01:48:59 +0530

    some changes

--- CourseApp/views.py
+++ CourseApp/views.py
@@ -227,8 +227,8 @@ class CourseAnalyticsAPI(APIView):
         to_date = self.request.GET.get('to_date',datetime.now().strftime("%Y-%m-%d"))
 
         amsterdam_timezone = pytz.timezone(TIME_ZONE)
-        from_date = amsterdam_timezone.localize(datetime.strptime(from_date, '%Y-%m-%d'))
-        to_date = amsterdam_timezone.localize(datetime.strptime(to_date, '%Y-%m-%d'))
+        from_date = amsterdam_timezone.localize(datetime.strptime(from_date+" 00:00:00", '%Y-%m-%d hh:mm:ss'))
+        to_date = amsterdam_timezone.localize(datetime.strptime(to_date+" 23:59:59", '%Y-%m-%d hh:mm:ss'))
 
         # qs = CoursePurchaseHistory.objects.all()
         qs = CoursePurchaseHistory.objects.filter(date_time__range=[from_date,to_date]).select_related('course').select_related('user')
